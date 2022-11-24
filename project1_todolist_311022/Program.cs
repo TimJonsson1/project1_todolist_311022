b6b2a38a6f88a734383cd28780bb621592484728
TimJonsson1 <tim.jonsson5@hotmail.com>
2022-11-24 14:43:10 +0100

    Add project files.

---
+++ project1_todolist_311022/Program.cs
@@ -0,0 +1,268 @@
+﻿
+using project1_todolist_311022;
+using System.Collections;
+using System.Linq;
+
+bool isRunning = true;
+List<Todolist> tasks = new List<Todolist>();
+
+tasks.Add(new Todolist("ToDo",new DateTime(2023,04,20),"not done","April Project"));
+tasks.Add(new Todolist("Other", new DateTime(2022,11,26), "not done", "November Project"));
+tasks.Add(new Todolist("A Letter", new DateTime(2022, 12, 08), "not done", "Aplha Project"));
+tasks.Add(new Todolist("B Letter", new DateTime(2023, 05, 01), "not done", "Alpha Project"));
+tasks.Add(new Todolist("C Letter", new DateTime(2022, 04, 15), "not done", "Aplha Project"));
+tasks.Add(new Todolist("D Letter", new DateTime(2022, 01, 01), "DONE", "Alpha Project"));
+// replace X and Y with amount later when you can read a file
+
+string title = "Title";
+string duedate = "Duedate";
+string status = "Status";
+string project = "Project";
+
+int spacing = 15;
+
+while (isRunning)
+{
+    Console.WriteLine("Welcome to ToDoList \n" +
+    "You have X tasks todo and Y taskt are done!\n" +
+    "Pick an option:\n" +
+    "(1) Show Task List (by date or project)\n" +
+    "(2) Add new Task\n" +
+    "(3) Edit Test (update, mark as done, remove)\n" +
+    "(4) Save and Quit\n");
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            //view all the tasks
+            viewTasks();
+            
+            break;
+
+        case "2":
+            //creating a task
+            createTask();
+            
+
+            break;
+
+        case "3":
+            //update an existing task
+            updateTask();
+            break;
+
+        case "4":
+            //quit the program
+            isRunning = false; 
+            break;
+
+
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("oops something went wrong please try to only use 1 - 4 as an option\n" +
+                "Press enter to continue");
+            Console.ReadLine();
+            printOptions();
+
+            break;
+    }
+
+
+
+}
+
+void printOptions()
+{
+    Console.ResetColor();
+    Console.WriteLine("\nWelcome to ToDoList \n" +
+    "You have X tasks todo and Y taskt are done!\n" +
+    "Pick an option:\n" +
+    "(1) Show Task List (by date or project)\n" +
+    "(2) Add new Task\n" +
+    "(3) Edit Test (update, mark as done, remove)\n" +
+    "(4) Save and Quit\n");
+}
+
+
+
+void viewTasks()
+{
+
+    DateTime dateNow = DateTime.Now;
+
+    Console.WriteLine("(1) sort by date - closes to Duedate or late first\n" +
+        "(2) sort by project A-Z");
+
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+
+            Console.WriteLine(title.PadRight(spacing) + " " + duedate.PadRight(spacing) + " " +
+           status.PadRight(spacing) + " " + project.PadRight(spacing) + "\n" +
+           title.Replace(title, "----- ").PadRight(spacing) + duedate.Replace(duedate, " ------- ").PadRight(spacing) +
+           status.Replace(status, "  ------").PadRight(spacing) + project.Replace(project, "   -------").PadRight(spacing) + "\n");
+
+            List<Todolist> sortedTodoListByDate = tasks.OrderBy(date => date.dueDate).ToList();
+
+            foreach (Todolist task in sortedTodoListByDate)
+            {
+                TimeSpan diff = task.dueDate - dateNow;
+                
+                if(diff.Days < 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                } 
+                else if (diff.Days < 20)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+                if (task.status.Equals("Done"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.WriteLine(task.title.PadRight(spacing) + " " + task.dueDate.ToString("dd/MM/yyyy").PadRight(spacing) + " " + task.status.PadRight(spacing) + " " + task.project.PadRight(spacing) + "\n");
+                Console.ResetColor();
+            }
+
+            break;
+                        
+        case"2":
+
+            Console.WriteLine(title.PadRight(spacing) + " " + duedate.PadRight(spacing) + " " +
+           status.PadRight(spacing) + " " + project.PadRight(spacing) + "\n" +
+           title.Replace(title, "----- ").PadRight(spacing) + duedate.Replace(duedate, " ------- ").PadRight(spacing) +
+           status.Replace(status, "  ------").PadRight(spacing) + project.Replace(project, "   -------").PadRight(spacing) + "\n");
+            
+            List<Todolist> sortedTodoListByProject = tasks.OrderBy(project => project.project).ToList(); 
+
+            foreach (Todolist task in sortedTodoListByProject)
+            {
+
+                TimeSpan diff = task.dueDate - dateNow;
+
+                if (diff.Days < 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (diff.Days < 20)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+                if (task.status.Equals("Done"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+
+                Console.WriteLine(task.title.PadRight(spacing) + " " + task.dueDate.ToString("dd/MM/yyyy").PadRight(spacing) + " " + task.status.PadRight(spacing) + " " + task.project.PadRight(spacing) + "\n");
+                Console.ResetColor();
+            }
+
+
+            break;
+
+            
+        default :
+            Console.WriteLine("Please choose (1) or (2) as the input");
+            break;
+
+    }
+
+    Console.WriteLine("press Enter to contine");
+    Console.ReadLine();
+
+    
+}
+
+void createTask()
+{
+    //Todolist task1 = new Todolist("Title","duedate","Status","Project");
+    //tasks.Add(task1);
+    
+    Console.Write("Add title: ");
+    string title = Console.ReadLine();
+    
+    Console.Write("Add duedate (dd/mm/yyyy): ");
+    string duedate = Console.ReadLine();
+    DateTime date = Convert.ToDateTime(duedate);
+    
+    Console.Write("Add Status(blank = not done): ");
+    string status = Console.ReadLine().ToUpper();
+    if (status.Equals(""))
+    {
+        status = "not done";
+    }
+    
+    Console.Write("Name of project project: ");
+    string project = Console.ReadLine();
+
+    Todolist task = new Todolist(title, date, status, project);
+    tasks.Add(task);
+
+
+}
+
+void updateTask()
+{
+    Console.WriteLine("select the number of the task you want to change");
+    Console.WriteLine("Title DueDate Status Project");
+    for (int i = 0; i < tasks.Count; i++)
+    {
+        Console.WriteLine("("+(i+1)+") " + tasks[i].title + " " + tasks[i].dueDate + " " + tasks[i].status + " " + tasks[i].project);
+    }
+
+    Console.WriteLine("select the task you want to update: ");
+    int taskChoice = int.Parse(Console.ReadLine());
+    taskChoice--;
+
+    Console.WriteLine("\nwhat do you want to change?\n" +
+        "(1) edit\n" +
+        "(2) mark as done\n" +
+        "(3) remove task");
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+
+            Console.WriteLine("\nwhat do you want to edit?\n" +
+            "(1) title\n" +
+            "(2) duedate\n" +
+            "(3) project name");
+
+            string editChoice = Console.ReadLine();
+
+            if (editChoice.Equals("1"))
+            {
+                Console.WriteLine("New title for Project: " + tasks[taskChoice].project + "\nWith Title: " + tasks[taskChoice].title);
+                string newTitle = Console.ReadLine();
+
+                Console.WriteLine("new Title is: " + newTitle + "\nif you regrett this choice press Q else press ENTER");
+
+                if (Console.ReadLine().ToUpper().Equals("Q"))
+                {
+
+                    break;
+                }
+                else
+                {
+                    tasks[taskChoice].title = newTitle;
+
+                }
+            }
+
+            break;
+            
+        case "2":
+            break;
+        
+        case "3":
+            break;
+    }
+    
+}
+
+
---
+++ project1_todolist_311022/Todolist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1_todolist_311022
+{
+    internal class Todolist
+    {
+        public Todolist(string title, DateTime dueDate, string status, string project)
+        {
+            this.title = title;
+            this.dueDate = dueDate;
+            this.status = status;
+            this.project = project;
+        }
+
+        public string title { get; set; }
+        public DateTime dueDate { get; set; }
+        public string status { get; set; }
+        public string project { get; set; }
+
+        public string printAll()
+        {
+            string all = "Title: " + this.title + "\n" + 
+                "Duedate: " + this.dueDate + "\n" + 
+                "Status: " + this.status + "\n" +
+                "project: " +this.project;
+            return all;
+        }
+    }
+}
